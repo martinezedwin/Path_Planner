5ea0e96c85482ef37888d01000a0758fe30fd573
Edwin Martinez <martinez92edwin@gmail.com>
2019-09-03 22:51:17 -0700

    Working on using spline to smooth path

--- src/main.cpp
+++ src/main.cpp
@@ -93,7 +93,7 @@ int main() {
           //   of the road.
           auto sensor_fusion = j[1]["sensor_fusion"];
 
-          int previous_x_size = previous_path_x.size(); //How many values previous x has
+          int previous_x_size = previous_path_x.size(); //How many values previous x has. Should be the same number of y values
 
           json msgJson;
 
@@ -104,6 +104,8 @@ int main() {
            * TODO: define a path made up of (x,y) points that the car will visit
            *   sequentially every .02 seconds
            */
+
+          /* //------------------Doing path using simple s and d values (Problem: too rigid, not good for the curves)--------------------------------
           double dist_inc = 0.4;
           double lane_width = 4.0; //Width of a lane from 
           for (int i = 0; i < 50; ++i) {
@@ -129,6 +131,86 @@ int main() {
             //std::cout<<"Car Speed: "<<car_speed<<std::endl;
 
           }
+          */
+
+          //Doing path using spline library (Attempt to smooth out the path)
+
+          //Init vector of points that will be used to create the spline
+          vector<double> spline_points_x;
+          vector<double> spline_points_y;
+
+          //Grab the x, y, and yaw measurements as reference
+          double ref_x = car_x;
+          double ref_y = car_y;
+          double ref_yaw = deg2rad(car_yaw); //IN RADIANS
+
+          //If prev_size is small, use the car's position as reference. If it's not, use the last number in previous_x_size
+          if(previous_x_size < 2){
+            //Define two points tangent to the path of car
+            double prev_car_x = car_x - cos(car_yaw);
+            double prev_car_y = car_y - sin(car_yaw);
+
+            //Add the previous tangent point and the current point to the points that will be used to create the spline
+            spline_points_x.push_back(prev_car_x);
+            spline_points_x.push_back(car_x);
+
+            spline_points_y.push_back(prev_car_y);
+            spline_points_y.push_back(car_y);
+          }
+          else{
+            //Use the last points on previous path as reference point
+            ref_x = previous_path_x[previous_x_size-1];
+            ref_y = previous_path_y[previous_x_size-1];
+            //Grab the second to last point as well
+            double ref_x_prev = previous_path_x[previous_x_size-2];
+            double ref_y_prev = previous_path_y[previous_x_size-2];
+            //use arctan to get reference yaw from these points
+            ref_yaw = atan2(ref_y-ref_y_prev, ref_x-ref_x_prev);
+
+            //Add the previous tangent point and the current point to the points that will be used to create the spline
+            spline_points_x.push_back(ref_x_prev);
+            spline_points_x.push_back(ref_x);
+
+            spline_points_y.push_back(ref_y_prev);
+            spline_points_y.push_back(ref_y);
+          }
+
+          //Grab 3 more points in Frenet coordinates, each 30 meters appart (30, 60, 90)
+          vector<double> next_wp0 = getXY(car_s+30, (2+4*lane), map_waypoints_s, map_waypoints_x, map_waypoints_y);
+          vector<double> next_wp1 = getXY(car_s+60, (2+4*lane), map_waypoints_s, map_waypoints_x, map_waypoints_y);
+          vector<double> next_wp2 = getXY(car_s+90, (2+4*lane), map_waypoints_s, map_waypoints_x, map_waypoints_y);
+
+          //add them to the list of points to create the spline
+          spline_points_x.push_back(next_wp0[0]);
+          spline_points_x.push_back(next_wp1[0]);
+          spline_points_x.push_back(next_wp2[0]);
+
+          spline_points_y.push_back(next_wp0[1]);
+          spline_points_y.push_back(next_wp1[1]);
+          spline_points_y.push_back(next_wp2[1]);
+
+          //Transform initial spline points from global to car's coordinates orientation
+          for(int i=0; i<spline_points_x.size(); i++){
+            double shift_x = spline_points_x[i] - ref_x;
+            double shift_y = spline_points_y[i] - ref_y;
+
+            spline_points_x[i] = (shift_x *cos(0-ref_yaw)-shift_y*sin(0-ref_yaw));
+            spline_points_y[i] = (shift_x *sin(0-ref_yaw)+shift_y*cos(0-ref_yaw));
+          }
+
+          //Init the spline
+          tk::spline s;
+
+          //Feed the x and y value of the points to the spline
+          s.set_points(spline_points_x, spline_points_y);
+
+          //Feed any previous points left over
+          for(int i=0; i<previous_path_x.size(); i++){
+            next_x_vals.push_back(previous_path_x[i]);
+            next_y_vals.push_back(previous_path_y[i]);
+
+          }
+
 
           msgJson["next_x"] = next_x_vals;
           msgJson["next_y"] = next_y_vals;
